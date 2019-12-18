0418ddcf48dc1f93c44fd12e5135230cc97122be
Arkin Modi <arkinmodi@gmail.com>
2019-12-17 19:51:03 -0500

    Updated privacy of methods

--- codebreaker.py
+++ codebreaker.py
@@ -4,7 +4,7 @@ from codemaker import *
 Color = ['red', 'green', 'blue', 'yellow', 'purple', 'white']
 
 class CodeBreaker:
-    def createSet(self):
+    def __createSet(self):
         codes = []
         for i in Color:
             for j in Color:
@@ -14,23 +14,22 @@ class CodeBreaker:
         return codes
 
 
-    def updateSet(self, result, guess, possibleCodes):
+    def __updateSet(self, result, guess, possibleCodes):
         i, end = 0, len(possibleCodes) - 1
         while i <= end:
-            if self.checkCode(possibleCodes[i], guess) != result: 
-                # print(checkCode(possibleCodes[i], guess), result)
+            if self.__checkCode(possibleCodes[i], guess) != result: 
                 possibleCodes.pop(i)
                 end = len(possibleCodes) - 1
             else: i += 1
 
 
-    def checkCode(self, code, guess):
+    def __checkCode(self, code, guess):
         black = sum(s == g for s, g in zip(code, guess))
         white = sum(min(guess.count(c), code.count(c)) for c in Color) - black
         return (black, white)
 
 
-    def minimax(self, master, possibleCodes):
+    def __minimax(self, master, possibleCodes):
         scoreAll = {}  
         scoreSingle = {}
         nextGuesses = [] 
@@ -38,7 +37,7 @@ class CodeBreaker:
         for i in master:
             for j in possibleCodes:
                 
-                pegs = self.checkCode(i, j)
+                pegs = self.__checkCode(i, j)
                 if pegs in scoreSingle: scoreSingle[pegs] += 1
                 else: scoreSingle[pegs] = 1
 
@@ -50,7 +49,7 @@ class CodeBreaker:
         return [list(code) for code in scoreAll if scoreAll[code] == least]
 
 
-    def findNextGuess(self, nextGuesses, master, possibleCodes):
+    def __findNextGuess(self, nextGuesses, master, possibleCodes):
         for i in nextGuesses:
             if list(i) in possibleCodes:
                 return i
@@ -62,18 +61,19 @@ class CodeBreaker:
 
     def run(self, CodeMaker):
         CodeMaker.newGame()
-        master = self.createSet()
+        master = self.__createSet()
         possibleCodes = master.copy()
         
         guess = [Color[0], Color[0], Color[1], Color[1]]
         result = CodeMaker.guessCode(guess)
-        if result == "win" or result == "lose": return result
+        if result == (4,0): yield (result, guess)
         
         for _ in range(11):
             if guess in possibleCodes: possibleCodes.remove(guess)
             if guess in master: master.remove(guess)
-            self.updateSet(result, guess, possibleCodes)
-            nextGuesses = self.minimax(master, possibleCodes)
-            guess = self.findNextGuess(nextGuesses, master, possibleCodes)
+            self.__updateSet(result, guess, possibleCodes)
+            nextGuesses = self.__minimax(master, possibleCodes)
+            guess = self.__findNextGuess(nextGuesses, master, possibleCodes)
             result = CodeMaker.guessCode(guess)
-            if result == "win" or result == "lose": return result
+            yield (result, guess)
+            if result == (4,0) or result == "lose": return
