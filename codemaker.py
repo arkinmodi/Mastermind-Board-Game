e737f19cd822c7278a8b41e3c2e76017dfa0ef32
Arkin Modi <arkinmodi@gmail.com>
2019-12-17 19:51:25 -0500

    Added getCode method and updated privacy

--- codemaker.py
+++ codemaker.py
@@ -3,20 +3,18 @@ from random import choice
 Colors = ['red', 'green', 'blue', 'yellow', 'purple', 'white']
 
 class CodeMaker:
-    def __init__(self):
-        self.code = None
-        self.attempts = None
-
     def newGame(self):
-        self.attempts = 0
-        self.code = [choice(Colors) for i in range(4)]
+        self.__attempts = 0
+        self.__code = [choice(Colors) for i in range(4)]
 
     def guessCode(self, guess):
         if type(guess) != list or len(guess) != 4 or not (set(guess) <= set(Colors)):
             return "invalid guess"
-        black = sum(s == g for s, g in zip(self.code, guess))
-        white = sum(min(guess.count(c), self.code.count(c)) for c in Colors) - black
-        self.attempts += 1
-        if black == 4: return "win"
-        if self.attempts == 12: return "lose"
+        black = sum(s == g for s, g in zip(self.__code, guess))
+        white = sum(min(guess.count(c), self.__code.count(c)) for c in Colors) - black
+        self.__attempts += 1
+        if self.__attempts == 12: return "lose"
         return (black, white)
+
+    def getCode(self):
+        return self.__code
